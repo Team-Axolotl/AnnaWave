d92588c614c95ad306d8cab73e7777eed731a928
Симо Андреев (Simo Andreev) <simeon.zlatanov.andreev@gmail.com>
2018-12-21 07:20:01 +0200

    fix: MISC - make the no-url-set placeholder url parsable construct.

--- app/src/main/java/bg/o/sim/annawave/storage/Settings.kt
+++ app/src/main/java/bg/o/sim/annawave/storage/Settings.kt
@@ -10,8 +10,8 @@ private const val SH_PREFS_KEY_BASE_URL = "baseUrl"
 private const val SH_PREFS_LANG = "lang"
 private const val DEFAULT_LANGUAGE = "en"
 private const val NO_LANGUAGE_SET = "N\\A"
-private fun getDefaultUrl() = HttpUrl.Builder().scheme("https").host("google.com").build()
-private fun noUrlSet() = HttpUrl.Builder().host("N\\A").build()
+private fun getDefaultUrl() = HttpUrl.Builder().scheme("http").host("192.168.1.7").port(19000).build()
+private fun noUrlSet() = HttpUrl.Builder().scheme("http").host("noUrlSet.placeholder").build()
 
 
 /** Values that need to be stored in-memory during runtime, and should be globally accessible go 'ere.*/
